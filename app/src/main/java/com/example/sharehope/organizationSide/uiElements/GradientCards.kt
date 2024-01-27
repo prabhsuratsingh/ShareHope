1788e1337380fde3e9b73534db1448da923e0e85
prabhsuratsingh <prabhsurat17@gmail.com>
2024-01-27 17:20:37 +0530

    Updated GradientCards uiElement
    added singleGradientBox composable

--- app/src/main/java/com/example/sharehope/organizationSide/uiElements/GradientCards.kt
+++ app/src/main/java/com/example/sharehope/organizationSide/uiElements/GradientCards.kt
@@ -1,11 +1,13 @@
 package com.example.sharehope.organizationSide.uiElements
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,26 +16,29 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.sharehope.R
 import com.example.sharehope.fontFamily
 import com.example.sharehope.ui.theme.C1
 import com.example.sharehope.ui.theme.C2
 import com.example.sharehope.ui.theme.C3
 import com.example.sharehope.ui.theme.C4
 
-
+val brush = Brush.verticalGradient(listOf(C1, C2, C3, C4))
 @Composable
 fun GradientCard(
     modifier: Modifier = Modifier,
 ) {
-
-    val brush = Brush.verticalGradient(listOf(C1, C2, C3, C4))
 
     Box (
         modifier = modifier
@@ -125,8 +130,48 @@ fun GradientCard(
     }
 }
 
+@Composable
+fun SingleGradientBox(
+    @StringRes heading: Int
+) {
+    Box (
+        modifier = Modifier
+            //.fillMaxSize()
+            .height(150.dp)
+            .padding(horizontal = 10.dp)
+            .shadow(
+                elevation = 10.dp,
+                shape = RoundedCornerShape(15.dp),
+                ambientColor = Color.DarkGray
+            )
+            .background(
+                brush = brush,
+                shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+        Text(
+            text = stringResource(heading),
+            style = TextStyle(
+                fontFamily = fontFamily,
+                color = Color.White,
+                fontSize = 20.sp
+            ),
+            modifier = Modifier
+                .padding(top = 18.dp, start = 18.dp)
+        )
+    }
+}
+
 @Preview
 @Composable
 fun DisplayGradientCard() {
     GradientCard()
+}
+
+@Preview
+@Composable
+fun DisplaySingleCard() {
+    SingleGradientBox(
+        heading = R.string.total_money_raised
+    )
 }
