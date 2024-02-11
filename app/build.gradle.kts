2f9dc1b28e484dc7e76a7a94306dde62ed3201e0
prabhsuratsingh <prabhsurat17@gmail.com>
2024-02-11 18:57:43 +0530

    Implemented Firebase Google Auth for OrgSide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -75,4 +76,17 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    //coil dependency
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
